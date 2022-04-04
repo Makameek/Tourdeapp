db5b962527ded0f4ea189154f10b1c452d0a4812
JanRacansky <91840038+JanRacansky@users.noreply.github.com>
2022-04-05 01:08:48 +0200

    Add files via upload

--- login.php
+++ login.php
@@ -10,21 +10,33 @@ $_SESSION["roles"] = 0;
 
 <html>
 <head>
-  <title>Login</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="styles.css">
+  <title>TDA Login</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
- <body>
-  <div>
-    <form action="login.php" method="post">
-     <label for="username">Uživatelské jméno:</label>
-     <input type="text" name="username" value="">
-      <br>
-     <label for="pass">Heslo:</label>
-     <input type="password" name="password" value="">
-      <br>
-     <input type="submit" value="Přihlásit se" name="login">
-    </form> 
-  </div>
- </body>
+  <body>
+    <div class = "siteBox">
+      <div class = "loginBox1">
+        <p class = "loginHeader">Přihlášení</p>
+        <div class = "loginBox2">
+          <form action="login.php" method="post">
+            <div class = "loginBox3">
+              <div class = "loginBox5">
+                <label for="username" class = "loginLabel">Uživatelské jméno</label>
+                <input type="text" name="username" class = "loginBox6" value="">
+              </div>
+              <div class = "loginBox1">
+                <label for="pass" class = "loginLabel">Heslo</label>
+                <input type="password" name="password" class = "loginBox6" value="">
+              </div>
+            </div>
+            <input type="submit" value="Přihlásit" name="login" class = "loginBox4">
+          </form>
+        </div>
+      </div>
+    </div>
+  </body>
 </html>
 
 <?php
